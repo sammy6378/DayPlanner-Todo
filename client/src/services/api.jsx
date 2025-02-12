4fa47a331a38daaf8112e9ea46583444f9ada891
sammy6378 <samuel202mwangi@gmail.com>
2025-02-12 10:53:54 +0300

    refactor: restructure project by removing old files and adding new components, configurations, and styles

---
+++ client/src/services/api.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+
+const API_Base_URL = 'http://localhost:8000';
+
+const apiClient = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
