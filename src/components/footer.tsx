ad6b565fd78bae02e732a6ed6b9550763b4f1b9b
sammy6378 <samuel202mwangi@gmail.com>
2025-02-12 10:25:36 +0300

    add footer component with copyright notice and Next.js branding

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='flex flex-col justify-center items-center py-2 bg-gray-800'>
+        <p>©️copyright 2024.  All rights reserved.</p>
+        <p>Powered by Next.js</p>
+    </footer>
+  )
+}
+
+export default Footer
