6de9f6221860b96335f1cdffec04f84f3271fa2f
sammy6378 <samuel202mwangi@gmail.com>
2025-02-12 10:26:05 +0300

    add home page component with introductory content

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Page() {
+    return (
+     <div className="flex flex-col justify-center items-center py-12">
+      <h2 className="text-3xl text-blue-400 font-semibold">Home Page </h2>
+      <p>Learning nextjs with page Router</p>
+     </div>
+    );
+  }
+  
