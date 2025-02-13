20e3aca7860c90ee9dca7d77cbccaf3d95ca5543
sammy6378 <samuel202mwangi@gmail.com>
2025-02-13 13:55:17 +0300

    feat: update database connection to use environment variable for MongoDB URL

--- api/utils/connect.ts
+++ api/utils/connect.ts
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
+import 'dotenv/config'
 
+// database url
+const url = process.env.MONGODB_URL as string;
 
-export const connectDb = (MONGO_URL: string) =>{
+export const connectDb = async () =>{
     try{
-        mongoose.connect(MONGO_URL);
+       await mongoose.connect(url);
         console.log('MongoDB Connected...');
     }catch(e){
         console.log('MongoDB Connect failed: ' + e.message);
