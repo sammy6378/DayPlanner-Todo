bf4ef5e2cea9396555d4ddcc20d0441c082c0451
sammy6378 <samuel202mwangi@gmail.com>
2025-02-13 13:30:52 +0300

    feat: add catchAsyncErrors middleware for handling async errors in routes

---
+++ api/middleware/catchAsyncErrors.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from "express";
+
+export const catchAsyncErrors =
+  (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+
+  
