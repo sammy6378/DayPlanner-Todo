045a37978ea081ae1237c2a9bd5284cf605e55cc
sammy6378 <samuel202mwangi@gmail.com>
2025-02-13 13:55:03 +0300

    refactor: update database connection import path and simplify connection call

--- api/server.ts
+++ api/server.ts
@@ -1,14 +1,11 @@
 import { app } from "./app";
 import express from 'express';
 import 'dotenv/config'
-import { connectDb } from "./db/connect";
+import { connectDb } from "./utils/connect";
 
 
 // port
 const port = process.env.PORT || 3000;
-
-// database url
-const url = process.env.MOGODB_URL as string;
 
 
 app.use(express.json());
@@ -17,6 +14,6 @@ app.use(express.json());
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
     // connect to MongoDB
-    connectDb(url);
+    connectDb();
 })
 
