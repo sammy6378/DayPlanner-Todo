0e79b6329b7bfe6d964058fa979fe650a72c8b8c
sammy6378 <samuel202mwangi@gmail.com>
2025-02-16 11:10:52 +0300

    feat: add login user function to auth service

--- client/src/services/authService.tsx
+++ client/src/services/authService.tsx
@@ -6,6 +6,11 @@ interface RegisterUserData {
   password: string;
 }
 
+interface loginData {
+  email: string;
+  password: string;
+}
+
 // Register user function
 export const registerUser = async (userData: RegisterUserData) => {
   try {
@@ -15,3 +20,14 @@ export const registerUser = async (userData: RegisterUserData) => {
     throw new Error(error.response?.data?.message || "Registration failed");
   }
 };
+
+
+// login user
+export const loginUser = async (userData: loginData) =>{
+try {
+    const response = await apiClient.post("/user/login", userData);
+    return response.data; // Return success response
+} catch (error: any) {
+    throw new Error(error.response?.data?.message || "Login failed");
+}
+}
