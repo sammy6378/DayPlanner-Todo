a72e41e09478bfd1c0ab60e68722d73c50374060
sammy6378 <samuel202mwangi@gmail.com>
2025-02-20 15:18:11 +0300

    refactor: remove unused Schema import from user model

--- api/models/usermodel.ts
+++ api/models/usermodel.ts
@@ -1,4 +1,4 @@
-import mongoose, {Document, Schema} from 'mongoose';
+import mongoose, {Document} from 'mongoose';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 export interface IUser extends Document {
