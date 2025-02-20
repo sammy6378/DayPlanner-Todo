7f5b323963abf0d1f2483157f50ddc03da120281
sammy6378 <samuel202mwangi@gmail.com>
2025-02-20 15:18:18 +0300

    refactor: rename loginUser to userLogin in user route for consistency

--- api/routes/user.route.ts
+++ api/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { activateUser, getUserInfo, loginUser, logoutUser, registerUser, setReminder } from '../controllers/user.controller';
+import { activateUser, getUserInfo, logoutUser, registerUser, userLogin,setReminder } from '../controllers/user.controller';
 
 const route = express.Router();
 
@@ -10,7 +10,7 @@ route.post('/user-register', registerUser);
 route.post('/activate-user', activateUser);
 
 // api/user/user-login  (Login)
-route.post('/user-login', loginUser);
+route.post('/user-login', userLogin);
 
 // api/user/user-logout (Logout)
 route.post('/user-logout', logoutUser);
