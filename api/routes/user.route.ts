7c6dd662adb916aa6cd6704755105964326d9f81
sammy6378 <samuel202mwangi@gmail.com>
2025-02-20 12:14:11 +0300

    feat: expand user routes to include activation, login, logout, and info retrieval

--- api/routes/user.route.ts
+++ api/routes/user.route.ts
@@ -1,9 +1,21 @@
 import express from 'express';
-import { registerUser } from '../controllers/user.controller';
+import { activateUser, getUserInfo, loginUser, logoutUser, registerUser } from '../controllers/user.controller';
 
 const route = express.Router();
 
+// api/user/user-register
 route.post('/user-register', registerUser);
 
+// api/user/activate-user
+route.post('/activate-user', activateUser);
+
+// api/user/user-login  (Login)
+route.post('/user-login', loginUser);
+
+// api/user/user-logout (Logout)
+route.post('/user-logout', logoutUser);
+
+// api/user/user-info
+route.get('/user-info', getUserInfo);
 
 export default route;
