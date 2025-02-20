2da282a87aa38b1857b3c2f7c256e46e26c9ba16
sammy6378 <samuel202mwangi@gmail.com>
2025-02-20 15:17:44 +0300

    feat: establish database connection on server startup

--- api/server.ts
+++ api/server.ts
@@ -1,7 +1,10 @@
 import { app } from "./app";
+import connectDB from "./utils/db";
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    connectDB();
 })
