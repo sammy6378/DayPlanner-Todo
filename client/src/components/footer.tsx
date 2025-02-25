d82a5d649e927f126f28b2323a9c931a4266c529
sammy6378 <samuel202mwangi@gmail.com>
2025-02-25 15:07:45 +0300

    style: update footer styling for improved theming and dark mode support

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='flex flex-col justify-center items-center py-2 bg-gray-800'>
+    <footer className='flex flex-col justify-center items-center border-t shadow-sm border-gray-400 bg-gray-100 py-2 dark:bg-gray-900 dark:text-white'>
         <p>©Copyright 2024. DayPlanner.  All rights reserved.</p>
     </footer>
   )
