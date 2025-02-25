976236d93d213ea7c6772cdf6054f5b52df01c26
sammy6378 <samuel202mwangi@gmail.com>
2025-02-25 15:07:10 +0300

    feat: integrate ThemeProvider for enhanced theming in dashboard layout

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -1,11 +1,12 @@
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
+import ThemeProvider from "@/utils/ThemeProvider";
 import React from 'react'
 
 
 function Layout({children}: {children: React.ReactNode}) {
   return (
-    <div>
+    <div  className={`dark:bg-gradient-to-b dark:from-gray-900 dark:to-black bg-white duration-300 bg-no-repeat min-h-screen w-full dark:text-white text-black`}>
     <Navbar />
     <div className='mt-6'>
       {children}
