e64146edb1465f6da4849c03e2d74c059e6b7d47
sammy6378 <samuel202mwangi@gmail.com>
2025-02-25 15:30:09 +0300

    style: enhance sidebar styles for improved dark mode support and readability

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -3,8 +3,8 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Sun, Moon, Calendar, PlusCircle, List, Settings, LogOut } from 'lucide-react';
-import '../app/app.css';
 import { useTheme } from "next-themes";
+import '../app/app.css';
 
 function Sidebar() {
   const router = useRouter();
@@ -29,11 +29,10 @@ function Sidebar() {
   if (!mounted) return null;
 
   return (
-    <>
-    <div className={`p-6  transform transition-transform duration-300 ease-in-out flex flex-col justify-between 
-    ${theme === "dark" ? "bg-gray-900" : "bg-gray-100"}`}>
+    <div className={`p-6 h-full transform transition-transform duration-300 ease-in-out flex flex-col justify-between 
+    ${theme === "dark" ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-800"}`}>
       <div>
-        <h2 className={`text-xl font-bold mb-6 ${theme === "dark" ? "text-white" : "text-gray-800"}`}>DayPlanner</h2>
+        <h2 className={`text-xl font-bold mb-6`}>DayPlanner</h2>
         
         <button className="sidebar-button" onClick={() => router.push('/dashboard/calendar')}>
           <Calendar /> <span>Calendar</span>
@@ -47,17 +46,17 @@ function Sidebar() {
           <List /> <span>Add Tasks</span>
         </button>
         
-        <button className="sidebar-button" >
+        <button className="sidebar-button">
           {theme === "light" ? (
             <Moon
               onClick={() => setTheme("dark")}
-              className="cursor-pointer text-black dark:text-white hover:text-slate-800 max-200px:w-[15px] max-200px:h-[15px]"
+              className="cursor-pointer text-black dark:text-white hover:text-slate-800"
               fill="black"
             />
           ) : (
             <Sun
               onClick={() => setTheme("light")}
-              className="cursor-pointer hover:text-slate-300 dark:text-white text-black max-200px:w-[15px] max-200px:h-[15px]"
+              className="cursor-pointer text-black dark:text-white hover:text-slate-300"
               fill="black"
             />
           )}
@@ -73,7 +72,6 @@ function Sidebar() {
         <LogOut /> <span>Logout</span>
       </button>
     </div>
-    </>
   );
 }
 
