ea688697ad3b9becbe0b6a639e09522f0256e0fd
sammy6378 <samuel202mwangi@gmail.com>
2025-02-25 13:54:43 +0300

    feat: enhance dashboard layout with improved padding and background color

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -6,15 +6,15 @@ import { CalendarPlus, ClipboardList } from "lucide-react";
 
 function Maindashboard() {
   return (
-    <div className="min-h-screen p-4  flex flex-col items-center text-center">
+    <div className="min-h-screen pt-16 px-6 flex flex-col items-center text-center bg-gray-900">
       {/* About Section */}
-      <h1 className="text-3xl font-bold text-slate-100">Welcome to DayPlanner</h1>
+      <h1 className="text-3xl font-bold text-slate-100">DayPlanner</h1>
       <p className="text-slate-200 mt-2 max-w-md">
         Stay organized and boost productivity with DayPlanner. Easily create events and plan your daily tasks in one place.
       </p>
 
       {/* Image */}
-      <div className="mt-6">
+      <div className="mt-6 flex justify-center">
         <Image
           src="/planner.jpeg" 
           alt="DayPlanner"
@@ -25,12 +25,12 @@ function Maindashboard() {
       </div>
 
       {/* Quick Actions */}
-      <div className="mt-8 w-full flex flex-col space-y-4">
-        <button className="flex items-center justify-center bg-blue-600 text-white py-3 rounded-lg w-full max-w-xs shadow-md hover:bg-blue-700">
+      <div className="mt-8 w-full flex flex-col items-center space-y-4">
+        <button className="flex items-center justify-center bg-blue-600 text-white py-3 px-4 rounded-lg w-full max-w-xs shadow-md hover:bg-blue-700">
           <CalendarPlus size={20} className="mr-2" /> Create Event
         </button>
 
-        <button className="flex items-center justify-center bg-green-600 text-white py-3 rounded-lg w-full max-w-xs shadow-md hover:bg-green-700">
+        <button className="flex items-center justify-center bg-green-600 text-white py-3 px-4 rounded-lg w-full max-w-xs shadow-md hover:bg-green-700">
           <ClipboardList size={20} className="mr-2" /> Plan Your Day with Tasks
         </button>
       </div>
