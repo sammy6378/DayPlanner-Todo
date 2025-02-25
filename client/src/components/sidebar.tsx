d359ec32a2bed53fa7b7fd5ed0a8fef276c018f8
sammy6378 <samuel202mwangi@gmail.com>
2025-02-25 15:08:14 +0300

    style: enhance sidebar for improved dark mode support and theming

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -1,26 +1,41 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Sun, Moon, Calendar, PlusCircle, List, Settings, LogOut } from 'lucide-react';
 import '../app/app.css';
-
+import { useTheme } from "next-themes";
 
 function Sidebar() {
   const router = useRouter();
-  const [darkMode, setDarkMode] = React.useState(false);
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const [active, setActive] = useState(false);
 
-  const toggleTheme = () => {
-    setDarkMode(!darkMode);
-    document.documentElement.classList.toggle('dark');
-  };
+  useEffect(() => {
+    if (window !== undefined) {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 80) {
+          setActive(true);
+        } else {
+          setActive(false);
+        }
+      });
+    }
+  }, []);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
 
   return (
-    <div className="bg-gray-100 dark:bg-gray-900 p-6 shadow-lg transform transition-transform duration-300 ease-in-out flex flex-col justify-between">
+    <>
+    <div className={`p-6  transform transition-transform duration-300 ease-in-out flex flex-col justify-between 
+    ${theme === "dark" ? "bg-gray-900" : "bg-gray-100"}`}>
       <div>
-        <h2 className="text-xl font-bold text-gray-800 dark:text-white mb-6">DayPlanner</h2>
+        <h2 className={`text-xl font-bold mb-6 ${theme === "dark" ? "text-white" : "text-gray-800"}`}>DayPlanner</h2>
         
-        <button className="sidebar-button " onClick={() => router.push('/dashboard/calendar')}>
+        <button className="sidebar-button" onClick={() => router.push('/dashboard/calendar')}>
           <Calendar /> <span>Calendar</span>
         </button>
         
@@ -28,16 +43,28 @@ function Sidebar() {
           <PlusCircle /> <span>Add Events</span>
         </button>
         
-        <button className="sidebar-button " onClick={() => router.push('/add-tasks')}>
+        <button className="sidebar-button" onClick={() => router.push('/add-tasks')}>
           <List /> <span>Add Tasks</span>
         </button>
         
-        <button className="sidebar-button " onClick={toggleTheme}>
-          {darkMode ? <Sun /> : <Moon />} 
-          <span>{darkMode ? 'Light Mode' : 'Dark Mode'}</span>
+        <button className="sidebar-button" >
+          {theme === "light" ? (
+            <Moon
+              onClick={() => setTheme("dark")}
+              className="cursor-pointer text-black dark:text-white hover:text-slate-800 max-200px:w-[15px] max-200px:h-[15px]"
+              fill="black"
+            />
+          ) : (
+            <Sun
+              onClick={() => setTheme("light")}
+              className="cursor-pointer hover:text-slate-300 dark:text-white text-black max-200px:w-[15px] max-200px:h-[15px]"
+              fill="black"
+            />
+          )}
+          <span>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</span>
         </button>
         
-        <button className="sidebar-button " onClick={() => router.push('/settings')}>
+        <button className="sidebar-button" onClick={() => router.push('/settings')}>
           <Settings /> <span>Settings</span>
         </button>
       </div>
@@ -46,6 +73,7 @@ function Sidebar() {
         <LogOut /> <span>Logout</span>
       </button>
     </div>
+    </>
   );
 }
 
